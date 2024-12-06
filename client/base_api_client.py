91b7ad16231818767d66a7f8f7d3986484b540d3
Merenda Franco N <merenda.franco83@gmail.com>
2024-12-06 19:37:30 +0100

    Removed unused file

--- client/base_api_client.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict
-
-import aiohttp
-
-
-class BaseAsyncClient:
-    base_url = "insert_base_url"
-    request_headers: Dict = {}
-    session: aiohttp.ClientSession = aiohttp.ClientSession()
