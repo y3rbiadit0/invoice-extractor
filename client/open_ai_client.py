07bfb76e857174a81f29e487563cfb1135fef1bb
Merenda Franco N <merenda.franco83@gmail.com>
2024-12-06 19:38:35 +0100

    Refactored client to make use of config

--- client/open_ai_client.py
+++ client/open_ai_client.py
@@ -1,31 +1,28 @@
-import os
-from typing import Dict
-
-from openai import AsyncOpenAI, AsyncStream
+from openai import OpenAI
+from openai.types.chat import ParsedChatCompletion
 
 from models import GenericInvoiceModel
-from .base_api_client import BaseAsyncClient
 from .open_ai_conf import OpenAIConfig
 
 
-class OpenAPIClient(BaseAsyncClient):
+class OpenAPIClient:
 
-    __open_ai_client: AsyncOpenAI
+    __open_ai_client: OpenAI
 
     def __init__(self):
-        api_key = os.getenv("OPEN_AI_API_KEY")
-        self.__open_ai_client = AsyncOpenAI(api_key=api_key)
+        self.__open_ai_client = OpenAI()
 
-    async def parsing_prompt(
+    def parsing_prompt(
         self,
         open_ai_config: OpenAIConfig,
-    ) -> AsyncStream:
+        raw_invoice_text: str,
+    ) -> ParsedChatCompletion:
         completion = self.__open_ai_client.beta.chat.completions.parse(
-            model="gpt-4o-2024-08-06",
+            model=open_ai_config.model,
             messages=[
-                {"role": "system", "content": open_ai_config.prompt_text},
-                {"role": "user", "content": "how can I solve 8x + 7 = -23"},
+                {"role": open_ai_config.role, "content": open_ai_config.prompt_text},
+                {"role": "user", "content": raw_invoice_text},
             ],
             response_format=GenericInvoiceModel,
         )
-        return await completion
+        return completion
