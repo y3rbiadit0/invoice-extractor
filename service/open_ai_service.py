882378c65a234a908d3b3ccbc756ce213054290e
Merenda Franco N <merenda.franco83@gmail.com>
2024-12-06 19:36:16 +0100

    Doing sync requests for the moment

--- service/open_ai_service.py
+++ service/open_ai_service.py
@@ -1,4 +1,3 @@
-import asyncio
 from typing import List
 
 from client.open_ai_client import OpenAPIClient
@@ -15,9 +14,6 @@ class OpenAIService:
         self._client = OpenAPIClient()
         self._config = OpenAIConfig()
 
-    def parse_invoice(self) -> List[GenericInvoiceModel]:
-        data = asyncio.run(self._client.parsing_prompt(open_ai_config=self._config))
-        import ipdb
-
-        ipdb.set_trace()
-        return data
+    def parse_invoice(self, raw_invoice_text: str) -> List[GenericInvoiceModel]:
+        data = self._client.parsing_prompt(open_ai_config=self._config, raw_invoice_text=raw_invoice_text)
+        return [data.choices[0].message.parsed]
