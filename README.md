442bc539c01ea5631c51cae1354bbbed5b79ae14
Merenda Franco N <merenda.franco83@gmail.com>
2024-12-06 22:23:03 +0100

    Updated dependencies and refactor main point entry

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 Extract data from invoices with different format/sizes to normalize the ingesture of data.
 
+## Setup Project
+1. Poetry 
 
+```dotenv
+INV_EXTRACTOR_INPUT_DATA=input/
+INV_EXTRACTOR_OUTPUT_DATA=output/
+OPENAPI_KEY=FILL_ME
+```
--- app.py
+++ app.py
@@ -1,44 +1,66 @@
+import logging
 import os
 from tkinter import Image
 from typing import List
 
+import dotenv
 import pytesseract
 from pdf2image import convert_from_path
 
 from models import GenericInvoiceModel
 from service.open_ai_service import OpenAIService
 
-
-def main():
-    files = files_to_process()
-    for file in files:
-        raw_data = extract_text(f"input_data/{file}")
-        invoice_model = parse_invoice(raw_data)
-        invoice_model.to_json()
-        invoice_model.to_xlsx()
+logger = logging.getLogger(__name__)
 
 
-def files_to_process(folder_path: str = "input_data"):
-    return [
-        f
-        for f in os.listdir(folder_path)
-        if os.path.isfile(os.path.join(folder_path, f))
-    ]
+class App:
+    _input_folder: str
+    _output_folder: str
 
+    def __init__(self):
+        self._set_logger_config()
+        self._load_environment_variables()
 
-def extract_text(file_path: str = "input_data") -> str:
-    images: List[Image] = convert_from_path(file_path)
-    text_data = ""
-    for image in images:
-        text = pytesseract.image_to_string(image)
-        text_data += text + "\n"
-    return text_data
+        self._input_folder = os.getenv("INV_EXTRACTOR_INPUT_DATA")
+        self._output_folder = os.getenv("INV_EXTRACTOR_OUTPUT_DATA")
+        if not os.path.exists(self._output_folder):
+            os.makedirs(self._output_folder)
 
+    def _load_environment_variables(self):
+        dotenv.load_dotenv()
 
-def parse_invoice(invoice_raw_data: str) -> GenericInvoiceModel:
-    open_ai_service = OpenAIService()
-    return GenericInvoiceModel()
+    def _set_logger_config(self):
+        logger.setLevel(logging.INFO)
+
+    def process_files(self):
+        files = self._get_input_files(self._input_folder)
+        for file in files:
+            raw_data = self._extract_text(f"{self._input_folder}/{file}")
+            logger.info("Processing file: %s", file)
+            invoice_model = self._parse_invoice(raw_data)[0]
+            invoice_model.file_name = file.split(".")[0]
+            invoice_model.to_json(output_folder=f"{self._output_folder}")
+            invoice_model.to_xlsx(output_folder=f"{self._output_folder}")
+
+    def _get_input_files(self, folder_path: str) -> List[str]:
+        return [
+            input_file
+            for input_file in os.listdir(folder_path)
+            if os.path.isfile(os.path.join(folder_path, input_file))
+        ]
+
+    def _extract_text(self, file_path: str = "input_data") -> str:
+        images: List[Image] = convert_from_path(file_path)
+        text_data = ""
+        for image in images:
+            text = pytesseract.image_to_string(image)
+            text_data += text + "\n"
+        return text_data
+
+    def _parse_invoice(self, invoice_raw_data: str) -> List[GenericInvoiceModel]:
+        invoice_articles = OpenAIService().parse_invoice(invoice_raw_data)
+        return invoice_articles
 
 
 if __name__ == "__main__":
-    main()
+    App().process_files()
